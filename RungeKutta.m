b6d151d990abf347e9a6570d45f46eb1c7c90cac
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2019-10-30 03:06:39 +0600

    Create RungeKutta.m

---
+++ RungeKutta.m
@@ -0,0 +1,13 @@
+## Author: Souhardya <Souhardya>
+## Created: 2019-10-30
+
+function Rungekutta(f, x0, y0, xmax, h)
+  f=inline(f);
+  while x0<xmax
+    k1 = h*f(x0, y0);
+    k2 = h*f(x0+h, y0+k1);
+    y0 = y0+0.5*(k1+k2);
+    x0=x0+h;
+    printf("%d %d\n",x0,y0);
+  endwhile
+endfunction
