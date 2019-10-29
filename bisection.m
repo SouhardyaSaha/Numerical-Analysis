f4e5fbdb835ad077eb3a2c7d614d5617f66e56db
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2019-10-29 21:13:39 +0600

    bisection method

---
+++ bisection.m
@@ -0,0 +1,35 @@
+## Author: Souhardya <Souhardya>
+## Created: 2019-10-29
+
+f = @(x) x*x*x + x*x - 1;
+low = input('low: ');
+high = input('high: ');
+dif = input('difference: ');
+if f(low)==0
+  fprintf("%f", low);
+  return;
+endif
+if f(high) == 0
+  fprintf("%f", high);
+  return;
+endif
+if (f(low)*f(high) > 0)
+  fprintf("don't have ans");
+  return;
+endif
+
+mid = (low+high)/2;
+fprintf("iterator   low            high             mid            value\n")
+i=1;
+ans = 1;
+while abs(f(mid))>=dif
+  mid = (low+high)/2;
+  fprintf("%d\t %f\t %f\t %f\t %f\t\n",i,low,high,mid,f(mid));
+  if(f(low)*f(mid)<0)
+    high = mid;
+  else
+    low = mid;
+  endif
+  ans = mid;
+endwhile
+fprintf("ans: %d\n", ans);
